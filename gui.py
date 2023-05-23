9e9f49684995a51224cde970ba22d772b37e6aa1
TungHD <>
2023-05-23 11:16:16 +0700

    Emplement the Complete Button

--- gui.py
+++ gui.py
@@ -7,10 +7,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+layout = [[label],
+          [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
+
 window = sg.Window('My To-Do App',
-                   layout=[[label],
-                           [input_box, add_button],
-                           [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 14))
 while True:
     event, values = window.read()
@@ -32,6 +37,16 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(todos)
             window['todo'].update("")
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            print(todo_to_complete)
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")
+        case "Exit":
+            break
         case "todos":
             selected_todo = values['todos'][0]
             window['todo'].update(selected_todo)

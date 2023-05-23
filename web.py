c71f5200b4f95b92e50424e2d9b53477d352f7c0
TungHD <>
2023-05-23 18:16:27 +0700

    Implement completed todo

--- web.py
+++ web.py
@@ -14,8 +14,13 @@ def get_todo():
     functions.write_todos(todos)
 
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="hello", placeholder="Enter new todo....", on_change=get_todo, key='new_todo')
 st.session_state
